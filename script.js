320c15ba416e8a0668945f048639b8cafa4773a8
wiseman2515 <135196684+wiseman2515@users.noreply.github.com>
2024-03-02 19:14:02 +0100

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the Friday!";
+  question.innerHTML = "Yay, see you on Friday!";
   gif.src =
     "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2g2dXpoZjU3OW5rZjU3MnlqODNrdjN3OGpndHFneXNpbWJldjlvbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/3o7btYzX9GycbDy7bW/giphy.gif";
 });
